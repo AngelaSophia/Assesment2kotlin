a3e93c1084672432b9b7b75adc3b8ffa46c58663
AngelaSophia <ayugisophiaangela@gmail.com>
2021-05-06 12:11:11 +0300

    sad

---
+++ src/assestest.kt
@@ -0,0 +1,40 @@
+fun main(){
+ //introduction("Ayugi",23)
+    dispenser()
+    arrayofNames("Ayugi","Brawlian","Laura")
+    println(arrayofNames())
+    Human("Sophia"23,56)
+
+}
+//fun introduction(name:String,age:Int){
+    //println(My name is $name and I am $age years old)
+//}
+fun dispenser(){
+   var age=0
+    if (age<=6){
+        println("a glass of milk")
+    }
+    else if (age>=6 && age<=15){
+        println("give a bottle of fanta")
+    }
+    else{
+        println("serve cocacola")
+    }
+}
+fun arrayofNames(num1:String,num2:String,num3:String):String{
+        return
+}
+fun multiply(){
+    var a=23
+    var b=20
+
+}
+
+
+class Human(name:String,age:Int,weight:Int){
+    var eat()
+
+}
+
+
+
